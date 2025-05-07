605d6f4b144294f469ae777662832d2721553a80
R0manOza <161681342+R0manOza@users.noreply.github.com>
2025-05-07 18:32:08 +0400

    changed port

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -7,7 +7,7 @@ import {
   Flashcard,
 } from "../types";
 
-const API_BASE_URL="http://localhost:3001/api"
+const API_BASE_URL="http://16.171.113.47:3001/api"
 const apiClient=axios.create({
     baseURL:API_BASE_URL,
     headers:{
